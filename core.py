7dbf042bb8f319a3180250c03b126f335c4b3418
CharmaineLin <xiaomin.lin.2015@mqf.smu.edu.sg>
2018-09-21 15:23:26 +0800

    Update core.py
    change database structure

--- core.py
+++ core.py
@@ -2,7 +2,7 @@
 """
 Created on Wed Sep 19 10:00:40 2018
 
-@author: charmaine
+@author: Charmaine
 """
 '''
 This is a world bank economic indicator data service.
@@ -11,31 +11,27 @@ There are mainly two parts, indicator_function and indicator api.
 import pymongo,json,time
 from urllib.request import urlopen
 import pandas as pd
+from bson import ObjectId
 from flask import Flask,request
-from flask_restplus import Resource, Api
+from flask_restplus import fields,Resource, Api
 
-#define api
-app = Flask(__name__)
-api = Api(app,
-          default='World Bank Economic Indicator',
-          title='World Bank Economic Indicator Data Service',
-          description='This is a data service for world bank economic indicator')
+
 
 #This part is the interaction from the api to database
 class Indicator_Function():
-    def __init__(self,uri):
+    def __init__(self,username,password,databases):
         #access to mongodb
+        uri = 'mongodb://%s:%s@%s'%(username,password,databases)
         client=pymongo.MongoClient(uri)        
-        self.db=client.get_database()
-        
-    #remove unused key, ObjectId created by mongo can't convert to a json element    
-    def remove_keys(self,target,poplist):
-        data=[]
-        for page in target:
-            for key in poplist:
-                page.pop(key) #pop keys from the page
-            data+=[page]
-        return data  
+        db=client.get_database()
+        self.collection=db['indicator']
+
+    #exist return 0, not exist return 1
+    def check_indicator_existence(self,indicator):
+        if type(self.collection.find_one({'indicator':indicator}))==dict:
+            return 200
+        else:
+            return 400
     
     #check whether indicator is valid    
     def check_validation(self,indicator):
@@ -44,10 +40,9 @@ class Indicator_Function():
         req=urlopen(url)
         content=str(req.read())
         if 'Invalid value' in content:
-            error=400
+            return 400
         else:
-            error=0
-        return error
+            return 0
 
     #get data from world bank
     def get_world_bank_data(self,indicator,page):
@@ -56,8 +51,7 @@ class Indicator_Function():
         url='http://api.worldbank.org/v2/'
         post_data='countries/%s/indicators/%s?date=%s&format=%s&page=%s'%('all',indicator,'2012:2017','json',page)
         req=urlopen(url+post_data)
-        content=req.read()
-        
+        content=req.read()        
         #retrieve expected value and store in a dict
         data=json.loads(content)[1]
         indicator_value=data[0]['indicator']['value']
@@ -65,171 +59,202 @@ class Indicator_Function():
         df['country']=[data[a]['country']['value'] for a in range(len(data))]
         df['date']=[data[a]['date'] for a in range(len(data))]
         df['value']=[data[a]['value'] for a in range(len(data))]
-        df_dict=df.to_dict(orient='records')
-        return indicator_value,df_dict
+        return indicator_value,df
 
     #import data into db
-    def import_database(self,collection,max_page):
-        indicator=collection
-        collection = self.db[collection]
+    def import_database(self,indicator,max_page):
+        data_dict={}
         #for loop to get data
         for page in range(1,max_page+1):
-            indicator_value,data=self.get_world_bank_data(indicator,page)  
-            creation_time=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))        
-            indicator_dict={'location':'/'+indicator+'/'+str(page),
-                            'collection_id':str(page),
-                            'indicator':indicator,
-                            'indicator_value':indicator_value,
-                            'creation_time':creation_time,
-                            'entries':data} #store data in a dict
-            collection.insert_one(indicator_dict) #insert to database
-        data=self.remove_keys(collection.find(),['_id','indicator_value','entries']) #remove _id and irrelevant information
+            indicator_value,data_dict[page]=self.get_world_bank_data(indicator,page)  
+        
+        data=pd.concat(data_dict).to_dict(orient='records')
+        creation_time=time.strftime('%Y-%m-%dT%H:%M:%SZ', time.localtime(time.time()))        
+        indicator_dict={'indicator':indicator,
+                        'indicator_value':indicator_value,
+                        'creation_time':creation_time,
+                        'entries':data} #store data in a dict
+        self.collection.insert_one(indicator_dict) #insert to database
+    
+    def data_format(self,data):
+        return {'location':'/indicators/'+str(data['_id']),
+                'collection_id':str(data['_id']),
+                'creation_time':data['creation_time'],'indicator':data['indicator']}
+    
+    #get data of the indicator
+    def get_indicator(self,indicator):
+        data=self.collection.find_one({'indicator':indicator})
+        return (self.data_format(data))    
+        
+    #get data of collection
+    def get_collection(self):
+        data=[]
+        for line in self.collection.find():
+            data.append(self.data_format(line))
         return data
-
-    #get collection
-    def get_collection(self,collection):
-        return self.remove_keys(self.db[collection].find(),['_id'])
-
     
     #get collection id
-    def get_collection_id(self,collection,collection_id):
-        data=self.remove_keys(self.db[collection].find({'collection_id':collection_id}),['_id'])
-        return data
+    def get_collection_id(self,collection_id):
+        try:
+            data=self.collection.find_one({'_id':ObjectId(collection_id)})           
+                
+            return ({'collection_id': str(data['_id']),
+                     'indicator': data['indicator'],
+                     'indicator_value': data['indicator_value'],
+                     'creation_time': data['creation_time'],
+                     'entries': data['entries']})
+        except:
+            return 400
     
     #delete collection id
-    def delete_collection_id(self,collection,collection_id):
-        self.db[collection].remove({'collection_id':collection_id})
+    def delete_collection_id(self,id):
+        try:
+            data=self.collection.find_one({'_id':ObjectId(id)})            
+            if type(data)==dict:
+                self.collection.remove({'_id':ObjectId(id)})
+#                data=self.collection.find_one({'_id':ObjectId(collection_id)}) 
+                return 200
+            return 400
+        except:
+            return 400
     
     #get collection, collection_id, date ,country
-    def get_date_country(self,collection,collection_id,date,country):
-        line=IF.db[collection].find_one({'collection_id':collection_id})
-        data=pd.DataFrame(line['entries'])
-        data=data[data.date==date] #get required date
-        data=data[data.country==country] #get required country
-        #if data length>0 return json, else return data
-        if len(data)>0:
-            data_json={'collection_id':collection_id,
-                       'indicator':line['indicator'],
-                       'country':country,
-                       'year':date,
-                       'value':data['value'].iloc[0]}
-            return data_json
-        else:
-            return data
+    def get_date_country(self,collection_id,date,country):
+        try:
+            line=self.collection.find_one({'_id':ObjectId(collection_id)})
+            data=pd.DataFrame(line['entries'])
+            data=data[data.date==date] #get required date
+            data=data[data.country==country] #get required country
+            return ({'collection_id':str(line['_id']),
+                     'indicator':line['indicator'],
+                     'country':country,
+                     'year':date,
+                     'value':data['value'].iloc[0]})
+        except:
+            return 400
+
         
     #get collection, collection_id, date ,country
-    def get_date(self,collection,collection_id,date,query):
-        line=IF.db[collection].find_one({'collection_id':collection_id}) #get target information
-        line.pop('_id') #remove _id
-        data=pd.DataFrame(line['entries']) #convert to dataframe
-        data=data[data.date==date] #get required data
-        data=data.sort_values('value',ascending=False) #sort
-        line.pop('entries') #remove old entries
-        #if query exists, get the slice according to the query
-        if query!='': 
-            query=query.lower() #change to lowercase
-            if 'top' in query:
-                data=data.iloc[:int(query[len('top'):])]
-            elif 'bottom' in query:
-                data=data.iloc[-int(query[len('bottom'):]):]
-                
-        line['entires']=data.to_dict(orient='records') #input new entries
-        if len(data)>0:
-            return line
-        else:
-            return data
-#    def test(self,collection,collection_id,date,query):
-#        b=IF.db[collection].find({'collection_id':collection_id})
-#        
-#        a=IF.db[collection].find({'entries.date':date},{'entries.$.date':1})
-#        for i in a:
-#            print(i)
-#        c=IF.db[collection].aggregate([{"$unwind":"$entries"}])
-#        d=IF.db[collection].aggregate([{"$unwind":"$entries"},
-#               {"$match":{"entries.date":date}}])
-#        
-#        e=IF.db[collection].aggregate([{"$unwind":"$entries"},
-#               {"$match":{"entries.date":date}},
-#               {"$project":{"entries":1}}])
-#        print('hello')
-           
-#/<string:collection> include get and post
-@api.route('/<string:collection>',methods=['GET','POST'],endpoint='collection_id')
+    def get_date(self,collection_id,date,query):
+        try:
+            line=self.collection.find_one({'_id':ObjectId(collection_id)}) #get target information
+            data=pd.DataFrame(line['entries']) #convert to dataframe
+            data=data[data.date==date] #get required data
+            data=data.sort_values('value',ascending=False) #sort
+            #if query exists, get the slice according to the query
+            if query!='': 
+                query=query.lower() #change to lowercase
+                if 'top' in query:
+                    data=data.iloc[:int(query[len('top'):])]
+                elif 'bottom' in query:
+                    data=data.iloc[-int(query[len('bottom'):]):]
+            return ({'indicator':line['indicator'],
+                     'indicator_value':line['indicator_value'],
+                     'entries':data.to_dict(orient='records')})
+        except:
+            return 400
+
+#define api
+app = Flask(__name__)
+api = Api(app,
+          default='World Bank Economic Indicator',
+          title='World Bank Economic Indicator Data Service',
+          description='This is a data service for world bank economic indicator')
+model = api.model('Indicator', {'indicator_id': fields.String})
+                    
+#/indicators include get and post
+@api.route('/indicators',methods=['GET','POST'],endpoint='collection_id')
 class Indicator_API_Collection(Resource):
     
     @api.response(200,'OK')
-    @api.response(400,'Inexistence')
-    def get(self,collection):
-        
-        data=IF.get_collection(collection)
-        if len(data)>0:
-            return data,200
-        else:
-            return {'message':'Collection={} do not exist in the database'.format(collection)},400
+    @api.doc(description='Retrieve the list of available collections')
+    def get(self):        
+        data=IF.get_collection()
+        return data,200
     
+    @api.expect(model)
     @api.response(200,'OK')
     @api.response(201,'Created')
-    @api.response(400,'Invalid')
-    def post(self,collection):
+    @api.response(400,'Error')
+    @api.doc(description='Import a collection from the data service')
+    def post(self):        
         
-        if len(IF.get_collection(collection))>0: 
-            return {'message':'Collection={} exist in database'.format(collection)},200
-        elif IF.check_validation(collection): 
-            return {'message':'Invalid indicator'},400
+        indicator=request.json['indicator_id']
+        
+        if IF.check_indicator_existence(indicator)==200: 
+            return {'message':'Collection={} exist in database'.format(indicator)},200
+        elif IF.check_validation(indicator)==400: 
+            return {'message':'Error'},400
         else:
-            data=IF.import_database(collection,2) #max two pages
+            IF.import_database(indicator,2) #max two pages
+            data=IF.get_indicator(indicator)
         return data,201
   
-#/<string:collection>/<string:collection_id> include get and delete
-@api.route('/<string:collection>/<string:collection_id>',methods=['GET','DELETE'])
+#/indicators/<string:collection_id> include get and delete
+@api.route('/indicators/<string:collection_id>',methods=['GET','DELETE'])
+@api.param('collection_id','collection id')
 class Indicator_API_Collection_ID(Resource):
+    
     @api.response(200,'OK')
-    @api.response(400,'Invalid')
-    def get(self,collection,collection_id):
-        data=IF.get_collection_id(collection,collection_id)
-        if len(data)>0:
-            return data
+    @api.response(400,'Error')
+    @api.doc(description='Retrieve a collection')
+    def get(self,collection_id):
+        data=IF.get_collection_id(collection_id)
+        if data==400:
+            return {'message':'Error'},400
         else:
-            return {'message':'Collection={} do not exist in the database'.format(collection+'/'+collection_id)},400
+            return data,200
         
     @api.response(200,'OK')
-    @api.response(400,'Invalid')
-    def delete(self,collection,collection_id):
-        if len(IF.get_collection_id(collection,collection_id))>0:
-            IF.delete_collection_id(collection,collection_id)
-            return {'message':'Collection={} is removed from the database!'.format(collection+'/'+collection_id)},200  
-        else:
-            return {'message':'Collection={} do not exist in the database'.format(collection+'/'+collection_id)},400
-            
-#/<string:collection>/<string:collection_id>/<string:date>/<string:country> include get
-@api.route('/<string:collection>/<string:collection_id>/<string:date>/<string:country>',methods=['GET'])
+    @api.response(400,'Error')
+    @api.doc(description='Deleting a collection with the data service')
+    def delete(self,collection_id):
+        flag=IF.delete_collection_id(collection_id)
+        if flag==400:
+            return {'message':'Error'},400
+        else:   
+            return {'message':'Collection=/indicators/{} is removed from the database!'.format(collection_id)},200  
+        
+#/indicators/<string:collection_id>/<string:year>/<string:country> include get
+@api.route('/indicators/<string:collection_id>/<string:year>/<string:country>',methods=['GET'])
+@api.param('collection_id', 'collection id')
+@api.param('year', 'given year')
+@api.param('country', 'given country')
 class Indicator_API_Country(Resource):
+    
     @api.response(200,'OK')
-    @api.response(400,'Inexistence')
-    def get(self,collection,collection_id,date,country):
-        if len(IF.get_collection_id(collection,collection_id))>0:
-            data=IF.get_date_country(collection,collection_id,date,country)
-            if len(data)>0:
-                return data,200
-        return {'message':'Collection={} do not exist in the database'.format(collection+'/'+collection_id+'/'+date+'/'+country)},400
+    @api.response(400,'Error')
+    @api.doc(description='Retrieve economic indicator value for given country and a year')
+    def get(self,collection_id,year,country):
+        data=IF.get_date_country(collection_id,year,country)
+        if data==400:
+            return {'message':'Error'},400
+        else:
+            return data,200
 
-#/<string:collection>/<string:collection_id>/<string:date> include get
-@api.route('/<string:collection>/<string:collection_id>/<string:date>',methods=['GET'])
-@api.param('query')
+
+#/indicators/<string:collection_id>/<string:year> include get
+@api.route('/indicators/<string:collection_id>/<string:year>',methods=['GET'])
+@api.param('collection_id', 'collection id')
+@api.param('year', 'given year')
+@api.param('query','top<n> or bottom<n>, n is the number of countries')
 class Indicator_API_Date(Resource):
+    
     @api.response(200,'OK')
-    @api.response(400,'Invalid entry')
-    def get(self,collection,collection_id,date):
+    @api.response(400,'Error')
+    @api.doc(description='Retrieve top/bottom economic indicator values for a given year')
+    def get(self,collection_id,year):
         query=request.args.get('query', '')
-        if len(IF.get_collection_id(collection,collection_id))>0:        
-            data=IF.get_date(collection,collection_id,date,query) 
-            if len(data)>0:
-                return data,200
-        return {'message':'Collection={} do not exist in the database'.format(collection+'/'+collection_id+'/'+date)},400
+        data=IF.get_date(collection_id,year,query)
+        if data==400:
+            return {'message':'Error'},400
+        else:
+            return data,200
 
 if __name__ == '__main__':
-    uri = 'mongodb://admin:admin123@ds261302.mlab.com:61302/wbei' #this is a temporary db
-    IF=Indicator_Function(uri)
-
+    username='admin' #database username
+    password='admin123' #database password
+    database='ds261302.mlab.com:61302/wbei' #database name
+    IF=Indicator_Function(username,password,database)
     # run the application
-    app.run(debug=True)        
+    app.run(debug=True)       
